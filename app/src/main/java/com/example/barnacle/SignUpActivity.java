c78e73301d842176c6600cad0abb0de9df9faa0a
wlals <s2019w26@e-mirim.hs.kr>
2020-11-09 01:59:22 +0900

    login and sign up

--- app/src/main/java/com/example/barnacle/SignUpActivity.java
+++ app/src/main/java/com/example/barnacle/SignUpActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -85,6 +86,70 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        //회원가입 버튼 클릭 시 수행
+        btn_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String userId = et_setId.getText().toString();
+                final String userPassword = et_setPassword.getText().toString();
+                final String userEmail = et_setEmail.getText().toString();
+                final String confirmPassword = et_confirmPassword.getText().toString();
+
+                //아이디 중복체크 확인
+                if (!validate){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    dialog = builder.setMessage("중복된 아이디가 있는지 확인하세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                //한 칸이라도 입력 안했을 경우
+                if (userId.equals("") || userPassword.equals("") || userEmail.equals("") || confirmPassword.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject( response );
+                            boolean success = jsonObject.getBoolean( "success" );
+
+                            //회원가입 성공시
+                            if(userPassword.equals(confirmPassword)) {
+                                if (success) {
+                                    Toast.makeText(getApplicationContext(), String.format("%s님 환영합니다.", userId), Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+
+                                    //회원가입 실패시
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                dialog = builder.setMessage("비밀번호가 동일하지 않습니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                                return;
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                //서버로 Volley를 이용해서 요청
+                SignUpRequest signUpRequest = new SignUpRequest(userId, userPassword, userEmail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue( SignUpActivity.this );
+                queue.add(signUpRequest);
+            }
+        });
 
         //버튼이 눌리면 activity_login 으로 돌아감
         btn_login.setOnClickListener(new View.OnClickListener() {
@@ -95,5 +160,6 @@ public class SignUpActivity extends AppCompatActivity {
 
             }
         });
+
     }
 }
