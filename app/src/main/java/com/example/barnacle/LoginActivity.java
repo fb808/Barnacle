009571ae7fd43f299dda21c4a68d6788d294de04
wlals <s2019w26@e-mirim.hs.kr>
2020-11-12 15:50:21 +0900

    구글로 로그인

--- app/src/main/java/com/example/barnacle/LoginActivity.java
+++ app/src/main/java/com/example/barnacle/LoginActivity.java
@@ -3,82 +3,67 @@ package com.example.barnacle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
-    SignInButton Google_Login;
-    private static final int RC_SIGN_IN = 100;
-    private FirebaseAuth mAuth;
-    private GoogleApiClient mGoogleApiClient;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
-        mAuth = FirebaseAuth.getInstance();
-        Google_Login = findViewById(R.id.Google_Login);
-        Google_Login.setOnClickListener(new View.OnClickListener() {
+        final TextView et_Id = (TextView) findViewById(R.id.et_Id);
+        final TextView et_Password = (TextView) findViewById(R.id.et_Password);
+        final Button btn_Login = (Button) findViewById(R.id.btn_login);
+        final Button btn_SignUp = (Button) findViewById(R.id.btn_SignUp);
+
+        final FirebaseAuth firebaseAuth =  FirebaseAuth.getInstance();
+
+        //가입 버튼이 눌리면
+        btn_SignUp.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-                startActivityForResult(signInIntent,RC_SIGN_IN);
+            public void onClick(View v) {
+                //intent함수를 통해 register액티비티 함수를 호출한다.
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            }
-            else{
-            }
-        }
-    }
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(),null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(MainActivity.this, "구글 로그인 인증 성공", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        //로그인 버튼이 눌리면
+        btn_Login.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String id = et_Id.getText().toString().trim();
+                String pwd = et_Password.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(id,pwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+
+                                }else{
+                                    Toast.makeText(LoginActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
 
     }
+
+
 }
--- app/src/main/java/com/example/barnacle/MainActivity.java
+++ app/src/main/java/com/example/barnacle/MainActivity.java
@@ -1,31 +1,40 @@
 package com.example.barnacle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView to_learn, self_check, write, map, ex_app;
+    Button to_login, to_learn, self_check, write, map, ex_app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        to_learn = (TextView) findViewById(R.id.to_learn);
+        to_login = (Button) findViewById(R.id.login_out);
+        to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        to_learn = (Button) findViewById(R.id.to_learn);
         to_learn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, 새로운.class);
+//                Intent intent = new Intent(MainActivity.this, dma.class);
 //                MainActivity.this.startActivity(intent);
             }
         });
 
-        self_check = (TextView) findViewById(R.id.self_check);
+        self_check = (Button) findViewById(R.id.self_check);
         self_check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        write = (TextView) findViewById(R.id.write);
+        write = (Button) findViewById(R.id.write);
         write.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        map = (TextView) findViewById(R.id.map);
+        map = (Button) findViewById(R.id.map);
         map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ex_app = (TextView) findViewById(R.id.ex_app);
+        ex_app = (Button) findViewById(R.id.ex_app);
         ex_app.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,6 +69,15 @@ public class MainActivity extends AppCompatActivity {
 //                MainActivity.this.startActivity(intent);
             }
         });
+
+//        Google_Login = (SignInButton) findViewById(R.id.Google_Login);
+////        Google_Login.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+////                MainActivity.this.startActivity(intent);
+////            }
+////        });
 
 
     }
