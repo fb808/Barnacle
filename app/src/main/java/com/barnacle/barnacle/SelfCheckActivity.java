ab959221609ff73bd60dce008c04891128043f11
wlals <s2019w26@e-mirim.hs.kr>
2020-11-17 15:36:05 +0900

    자가진단 결과 창으로 띄우기

--- app/src/main/java/com/barnacle/barnacle/SelfCheckActivity.java
+++ app/src/main/java/com/barnacle/barnacle/SelfCheckActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,9 +15,10 @@ public class SelfCheckActivity extends AppCompatActivity {
 
     ImageView imageView;
     EditText birth;
+    TextView txtResult;
     CheckBox checkBox1, checkBox2, checkBox3, checkBox4, checkBox5;
     CheckBox checkBox6, checkBox7, checkBox8, checkBox9, checkBox10;
-    CheckBox checkBox11, checkBox12, checkBox13;
+    CheckBox checkBox11, checkBox12;
     Button submit;
     private String[] vaccin = new String[]
             {"인플루엔자(Flu)", "폐렴구균(PPSV)", "폐렴구균(PCV)", "파상풍/디프테리아(백일해)/(Tdap/Td)", "A형간염(HepA)",
@@ -40,115 +42,79 @@ public class SelfCheckActivity extends AppCompatActivity {
 
         });
 
-        birth = (EditText) findViewById(R.id.birth);
+//        setOfCheckBoxes = new CheckBox[]{
+//            (CheckBox) findViewById(R.id.checkBox1),
+//            (CheckBox) findViewById(R.id.checkBox2),
+//            (CheckBox) findViewById(R.id.checkBox3),
+//            (CheckBox) findViewById(R.id.checkBox4),
+//            (CheckBox) findViewById(R.id.checkBox5),
+//            (CheckBox) findViewById(R.id.checkBox6),
+//            (CheckBox) findViewById(R.id.checkBox7),
+//            (CheckBox) findViewById(R.id.checkBox8),
+//            (CheckBox) findViewById(R.id.checkBox9),
+//            (CheckBox) findViewById(R.id.checkBox10),
+//            (CheckBox) findViewById(R.id.checkBox11),
+//            (CheckBox) findViewById(R.id.checkBox12),
+//            (CheckBox) findViewById(R.id.checkBox13)
+//        };
 
-        setOfCheckBoxes = new CheckBox[]{
-            (CheckBox) findViewById(R.id.checkBox1),
-            (CheckBox) findViewById(R.id.checkBox2),
-            (CheckBox) findViewById(R.id.checkBox3),
-            (CheckBox) findViewById(R.id.checkBox4),
-            (CheckBox) findViewById(R.id.checkBox5),
-            (CheckBox) findViewById(R.id.checkBox6),
-            (CheckBox) findViewById(R.id.checkBox7),
-            (CheckBox) findViewById(R.id.checkBox8),
-            (CheckBox) findViewById(R.id.checkBox9),
-            (CheckBox) findViewById(R.id.checkBox10),
-            (CheckBox) findViewById(R.id.checkBox11),
-            (CheckBox) findViewById(R.id.checkBox12),
-            (CheckBox) findViewById(R.id.checkBox13)
-        };
-//
 //        for(int i = 0; i < setOfCheckBoxes.length; i++){
 //            setOfCheckBoxes[i].setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);
 //        }
 
-//        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
-//        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
-//        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
-//        checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
-//        checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
-//        checkBox6 = (CheckBox) findViewById(R.id.checkBox6);
-//        checkBox7 = (CheckBox) findViewById(R.id.checkBox7);
-//        checkBox8 = (CheckBox) findViewById(R.id.checkBox8);
-//        checkBox9 = (CheckBox) findViewById(R.id.checkBox9);
-//        checkBox10 = (CheckBox) findViewById(R.id.checkBox10);
-//        checkBox11 = (CheckBox) findViewById(R.id.checkBox11);
-//        checkBox12 = (CheckBox) findViewById(R.id.checkBox12);
-//        checkBox13 = (CheckBox) findViewById(R.id.checkBox13);
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
+        checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
+        checkBox6 = (CheckBox) findViewById(R.id.checkBox6);
+        checkBox7 = (CheckBox) findViewById(R.id.checkBox7);
+        checkBox8 = (CheckBox) findViewById(R.id.checkBox8);
+        checkBox9 = (CheckBox) findViewById(R.id.checkBox9);
+        checkBox10 = (CheckBox) findViewById(R.id.checkBox10);
+        checkBox11 = (CheckBox) findViewById(R.id.checkBox11);
+        checkBox12 = (CheckBox) findViewById(R.id.checkBox12);
 
-        disease = new boolean[13];
-        final String[] result = {""};
+//        disease = new boolean[13];
+//        String result = "";
+//
+//        for (int i = 0; i < setOfCheckBoxes.length; i++) {
+//            disease[i] = setOfCheckBoxes[i].isChecked();
+//        }
 
-        for (int i = 0; i < setOfCheckBoxes.length; i++) {
-            disease[i] = setOfCheckBoxes[i].isChecked();
-        }
+        txtResult = (TextView)findViewById(R.id.txtResult);
 
-//        int now_year = 2020;
-//        birth_num = parseInt(birth.getText().toString()); //정수값 가져오기
+//        birth = (EditText) findViewById(R.id.birth);
+//        birth_num = Integer.parseInt(birth.getText().toString());
+//
+//        long now = System.currentTimeMillis();
+//        Date mDate = new Date(now);
+//        SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy");
+//        String getTime = simpleDate.format(mDate);
+//        int timeNow = Integer.parseInt(getTime);
+//
+//        final int age = (timeNow - birth_num + 1);
 
-//        submit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (disease[0]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1]);
-//                }
-//
-//                if (disease[1]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1]);
-//                }
-//
-//                if (disease[2]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1]);
-//                }
-//
-//                if (disease[3]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[5]);
-//                }
-//
-//                if (disease[4]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[4] + ", \n" + vaccin[5]);
-//                }
-//
-//                if (disease[5]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[2]);
-//                }
-//
-//                if (disease[6]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[2]);
-//                }
-//
-//                if (disease[7]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[2]);
-//                }
-//
-//                if (disease[8]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[2] + ", \n" + vaccin[3] + ", \n"
-//                            + vaccin[4] + ", \n" + vaccin[5] + ", \n" + vaccin[10] + ", \n" + vaccin[11]);
-//                }
-//
-//                if (disease[9]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[2] + ", \n" + vaccin[9] + ", \n" + vaccin[10]);
-//
-//                }
-//
-//                if (disease[10]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[5]);
-//
-//                }
-//
-//                if (disease[11]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[1] + ", \n" + vaccin[5]);
-//
-//                }
-//
-//                if (disease[12]) {
-//                    result[0] += (vaccin[0] + ", \n" + vaccin[3]);
-//
-//                }
-//
-//                TextView tv = (TextView) findViewById(R.id.result_selfCheck);
-//                tv.setText(result[0]);
-//            }
-//        });
    }
+
+    //버튼
+    public void mOnPopupClick(View v){
+        //데이터 담아서 팝업(액티비티) 호출
+        Intent intent = new Intent(this, PopupActivity.class);
+        intent.putExtra("data", "Test Popup");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //데이터 받기
+                String result = data.getStringExtra("result");
+                txtResult.setText(result);
+            }
+        }
+    }
+
 }
